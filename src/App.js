7752c255fba65b33f8ff67064c17540d00d6bfe0
dhiami <dhiamissaoui12121@gmail.com>
2021-10-30 22:55:14 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  incrementHandler,
+  decrementHandler,
+  resetHandler,
+} from "./Redux/action";
+
+function App() {
+  const dispatch = useDispatch();
+  const plus = () => {
+    dispatch(incrementHandler());
+  };
+  const minus = () => {
+    dispatch(decrementHandler());
+  };
+  const count = useSelector((state) => state.counter);
+  console.log(count);
+  return (
+    <div className="App">
+      <h1> Redux Counter Workshop</h1>
+      <div className="counter">
+        <button onClick={plus}> +</button>
+        <h3> {count} </h3>
+        <button onClick={minus}> -</button>
+      </div>
+      <button onClick={() => dispatch(resetHandler())}> Reset </button>
+    </div>
+  );
+}
+
+export default App;
